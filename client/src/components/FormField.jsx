3f6ba1f9caadcb42f42d793f163ea8f38b3ce2f7
Son Dao <udemydaohsn@gmail.com>
2023-04-19 10:10:57 -0700

    finsh the front end UI

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -29,6 +29,16 @@ const FormField = ({
           </button>
         )}
       </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+      />
     </>
   );
 };
