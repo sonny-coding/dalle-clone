06ef675a6fb57e7a3d989bcdd30de80072d351c6
Son Dao <udemydaohsn@gmail.com>
2023-04-20 15:37:41 -0700

    check for .gitignore

--- server/index.js
+++ server/index.js
@@ -3,22 +3,19 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
-import dalleRoutes from "./routes/dalleRoutes.js ";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(
-  express.json({
-    limit: "50mb",
-  })
-);
-app.use("api/v1/post", postRoutes);
-app.use("api/v1/dalle", dalleRoutes);
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
-  res.send("hello from DALL-E");
+  res.send("hello from DALL-E clone");
 });
 
 const startServer = async () => {
